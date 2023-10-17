fdae33d897fb35ba8920bfac45bf74bf42531b3f
Munkh Erdene <m.erdene404@gmail.com>
2023-10-16 20:09:45 -0400

    first commit

---
+++ mergeSortedArray.js
@@ -0,0 +1,30 @@
+var merge = function(nums1, m, nums2, n) {
+    let p1 = m - 1; // Pointer for nums1
+    let p2 = n - 1; // Pointer for nums2
+    let p = m + n - 1; // Pointer for the merged array
+
+    while (p1 >= 0 && p2 >= 0) {
+        if (nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+        } else {
+            nums1[p] = nums2[p2];
+            p2--;
+        }
+        p--;
+    }
+
+    while (p2 >= 0) {
+        nums1[p] = nums2[p2];
+        p2--;
+        p--;
+    }
+};
+
+const nums1 = [1, 2, 3, 0, 0, 0];
+const m = 3;
+const nums2 = [2, 5, 6];
+const n = 3;
+
+merge(nums1, m, nums2, n);
+console.log(nums1); // Output: [1, 2, 2, 3, 5, 6]
